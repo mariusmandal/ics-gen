1155295a583b1257de7c65aada3f078cee033f32
Marius Mandal <mariusmandal@gmail.com>
2014-10-16 18:36:20 +0200

    Basic support
    Still missing tons of functionality.
    Cannot use uniqueID (Gives calendar alert at every sync)

--- ICS/Calendar.php
+++ ICS/Calendar.php
@@ -1,14 +1,11 @@
 <?php
-/* 
-Class Name: ICS_Calendar
-Class URI: https://github.com/mariusmandal/ics-gen
-Author: Marius Mandal 
-Version: 0.1 
-Author URI: http://www.mariusmandal.no
-*/
-class ICS_Calendar {
-	
+namespace ICS;
+
+class Calendar {
+	var $filepath = 'ics-gen-temp/noname.ics';
 	var $name;
+	var $prodid = '-//mariusmandal.no//ics-gen//NO';
+	var $calendar_type = 'GREGORIAN';
 	
 	var $events = [];
 
@@ -16,24 +13,54 @@ class ICS_Calendar {
 		$this->name = $name;
 	}
 	
+	public function setDescription( $description ) {
+		$this->description = $description;
+	}
+	
+	public function setOwner( $owner, $website, $language='NO' ) {
+		$this->prodid = '-//'. $website .'//'. $owner .'//'. $owner;
+	}
+	
 	public function addEvent( $event ) {
 		$this->events[] = $event;
 	}
 	
-	public function write( $filename ) {
+	public function write( $full_path, $download_url ) {
+		$this->filepath = $full_path;
+		
 		$this->_fopen();
 		$this->_writeHeader();
 		$this->_writeEventList();
 		$this->_writeFooter();
 		$this->_fclose();
+		
+		return $download_url;
 	}
+	
+	private function _writeEventList() {
+		foreach( $this->events as $event ) {
+			fwrite( $this->file, $event->write() );
+		}
+	}
+	
 	
 	private function _writeHeader() {
-		
+		fwrite( $this->file, 
+			 'BEGIN:VCALENDAR' ."\r\n"
+			.'VERSION:2.0' ."\r\n"
+			.'CALSCALE:'. $this->calendar_type ."\r\n"
+			.'METHOD:PUBLISH' ."\r\n"
+			.'X-WR-CALNAME:'. $this->name ."\r\n"
+			.'PRODID:'. $this->prodid ."\r\n"
+			);
 	}
 	
-	private function _fopen( $filename ) {
-		$this->file = fopen( $filename, 'w' );
+	private function _writeFooter() {
+		fwrite( $this->file, 'END:VCALENDAR' ."\r\n" );
+	}
+	
+	private function _fopen( ) {
+		$this->file = fopen( $this->filepath, 'w' );
 	}
 	
 	private function _fclose() {
--- ICS/Event.php
+++ ICS/Event.php
@@ -1,36 +1,70 @@
 <?php
+namespace ICS;
 /* 
-Class Name: ICS_Event
+Class title: ICS_Event
 Class URI: https://github.com/mariusmandal/ics-gen
 Author: Marius Mandal 
 Version: 0.1 
 Author URI: http://www.mariusmandal.no
 */
-class ICS_Event {
+
+use DateTime;
+
+class Event {
 	
-	var $name;
+	var $title;
 	var $start;
 	var $stop;
-	var $description;
+	var $description = '';
+	var $location = '';
+	var $owner = 'marius@mariusmandal.no';
+	var $status = 'CONFIRMED';
+	
+	private $dateFormat = 'Ymd';
 
-	public function __construct( $name ) {
-		$this->name = $name;
+	public function __construct( $title ) {
+		$this->title = $title;
+		$this->start = new DateTime();
+		$this->stop = new DateTime();
 	}
 	
-	public function setName( $name ) {
-		$this->name = $name;
+	public function setTitle( $title ) {
+		$this->title = $title;
+		return $this;
 	}
 	
 	public function setStart( $start ) {
 		$this->start = $start;
+		return $this;
 	}
 	
 	public function setStop( $stop ) {
 		$this->stop = $stop;
+		return $this;
 	}
 	
 	public function setDescription( $description ) {
 		$this->description = $description;
+		return $this;
+	}
+	
+	public function setStatus( $status ) {
+		$this->status = $status;
+	}
+	
+	public function write() {
+		$event = 'BEGIN:VEVENT' ."\r\n"
+				.'DTSTART;VALUE=DATE:'. $this->start->format( $this->dateFormat ) ."\r\n"
+				.'DTEND;VALUE=DATE:'. $this->stop->format( $this->dateFormat ) ."\r\n"
+				.'SUMMARY:'. $this->title ."\r\n"
+				.'LOCATION: '. $this->location ."\r\n"
+				.'UID: '. $this->owner .'-'.uniqid() ."\r\n"
+				.'SEQUENCE:0' ."\r\n"
+				.'DESCRIPTION:'. $this->description ."\r\n"
+				.'STATUS:'. $this->status ."\r\n"
+				.'END:VEVENT' ."\r\n"
+				;
+		return $event;
 	}
 	
 }
